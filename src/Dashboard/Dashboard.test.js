243c126a4757e358b335e74519273945dd79d1bd
Rifki.Nurfaiz <rifki.nurfaiz@btpn.com>
2018-10-02 17:34:09 +0700

    Update dashboard as customer wants

--- src/Dashboard/Dashboard.test.js
+++ src/Dashboard/Dashboard.test.js
@@ -1,35 +1,35 @@
 import React from 'react';
-import {shallow} from 'enzyme';
+import { shallow } from 'enzyme';
 import Dashboard from './Dashboard';
 
 describe('Dashboard', () => {
-    function renderDashboard(customer) {
-        return shallow(<Dashboard customer={customer}/>);
-    }
+  function renderDashboard(customer) {
+    return shallow(<Dashboard customer={customer} />);
+  }
 
-    describe('render', () => {
-        it('should has one div', () => {
-            const customer = {account: {amount: 0}};
-            const dashboard = renderDashboard(customer);
-            expect(dashboard.find('#dashboard').length).toEqual(1);
-        });
-
-        it('should has customer name', () => {
-            const customer = {account: {amount: 0}};
-            const dashboard = renderDashboard(customer);
-            expect(dashboard.find('.customer-name').length).toEqual(1);
-        });
-
-        it('should has customer balance', () => {
-            const customer = {account: {amount: 0}};
-            const dashboard = renderDashboard(customer);
-            expect(dashboard.find('#customer-balance').length).toEqual(1);
-        });
-
-        it('should has transaction list', () => {
-            const customer = {account: {amount: 0}};
-            const dashboard = renderDashboard(customer);
-            expect(dashboard.find('#transaction-list').length).toEqual(1);
-        });
+  describe('render', () => {
+    it('should has one div', () => {
+      const customer = { account: { amount: 0 } };
+      const dashboard = renderDashboard(customer);
+      expect(dashboard.find('#dashboard').length).toEqual(1);
     });
+
+    it('should has customer name', () => {
+      const customer = { account: { amount: 0 } };
+      const dashboard = renderDashboard(customer);
+      expect(dashboard.find('.customer-name').length).toEqual(1);
+    });
+
+    it('should has customer balance', () => {
+      const customer = { account: { amount: 0 } };
+      const dashboard = renderDashboard(customer);
+      expect(dashboard.find('#customer-balance').length).toEqual(1);
+    });
+
+    it('should has transaction list', () => {
+      const customer = { account: { amount: 0 } };
+      const dashboard = renderDashboard(customer);
+      expect(dashboard.find('#transaction-list').length).toEqual(1);
+    });
+  });
 });
