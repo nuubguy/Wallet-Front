fcf445554c92f151b7f07078ac2638e85be43c85
Rifki.Nurfaiz <rifki.nurfaiz@btpn.com>
2018-10-02 17:33:35 +0700

    Better view for transaction history

--- src/DetailTransaction/DetailTransactionContainer.js
+++ src/DetailTransaction/DetailTransactionContainer.js
@@ -4,24 +4,29 @@ import AccountService from "../Api/AccountService";
 import Customer from "../Utilities/Constant";
 import Endpoint from "../Api/Endpoint";
 import TransactionList from "../Dashboard/TransactionList";
+import './DetailTransaction.css'
+import Notifications, {notify} from 'react-notify-toast';
+
 //TODO create paging for transaction and save description for sorting
+
 export default class DetailTransactionContainer extends Component {
     constructor() {
         super();
         this.state = {
-            transactions:[],
-            description:'',
-            amount:'',
-            sort:0,
-            notFoundMessage:'',
-            open:false,
+            transactions: [],
+            description: '',
+            amount: '',
+            sort: 0,
+            notFoundMessage: '',
+            open: false,
         };
     }
 
-    async componentDidMount(){
+    async componentDidMount() {
         const transactions = await this.inputValidation();
-        if (transactions.data.length===0){
-            this.onOpenModal()
+        if (transactions.data.length === 0) {
+            let color = {background: '#EB4D4B', text: "#FFFFFF"};
+            notify.show("Transaction not found", 'custom', 5000, color);
         }
         this.setState({
             transactions: transactions.data
@@ -29,17 +34,17 @@ export default class DetailTransactionContainer extends Component {
     }
 
 
-    inputValidation(){
+    inputValidation() {
         const service = new AccountService(Customer.id(), Customer.accountId(), Endpoint.baseUrl());
         let transactions = service.getAllTransactionList(this.state.sort)
-        if(this.state.description!==''&&this.state.amount===''){
-            transactions =service.getTransactionListBasedOnDescription(this.state.description,this.state.sort)
+        if (this.state.description !== '' && this.state.amount === '') {
+            transactions = service.getTransactionListBasedOnDescription(this.state.description, this.state.sort)
         }
-        if (this.state.description===''&&this.state.amount!==''){
-            transactions = service.getTransactionListBasedOnAmount(parseFloat(this.state.amount),this.state.sort)
+        if (this.state.description === '' && this.state.amount !== '') {
+            transactions = service.getTransactionListBasedOnAmount(parseFloat(this.state.amount), this.state.sort)
         }
-        if (this.state.description!==''&& this.state.amount!==''){
-            transactions = service.getTransactionListBasedOnAmountAndDescription(parseFloat(this.state.amount),this.state.description,
+        if (this.state.description !== '' && this.state.amount !== '') {
+            transactions = service.getTransactionListBasedOnAmountAndDescription(parseFloat(this.state.amount), this.state.description,
                 this.state.sort)
         }
         return transactions
@@ -47,66 +52,59 @@ export default class DetailTransactionContainer extends Component {
 
     render() {
         return (
-            <section>
-                <DetailTransactionInput
-                amount={this.state.amount}
-                amountOnChange={this.amountOnChange}
-                description={this.state.description}
-                descriptionOnChange={this.descriptionOnChange}
-                formSubmit={this.formSubmit}
-                imageOnClick={this.imageOnClick}
-                notFoundMessage={this.state.notFoundMessage}
-                onOpenModal={this.onOpenModal}
-                onCloseModal={this.onCloseModal}
-                open={this.state.open}
-                />
-                <TransactionList transactions={this.state.transactions}/>
-            </section>
+            <div>
+                <Notifications/>
+                <section className={"body"}>
+                    <DetailTransactionInput
+                        amount={this.state.amount}
+                        amountOnChange={this.amountOnChange}
+                        sort={this.state.sort}
+                        description={this.state.description}
+                        descriptionOnChange={this.descriptionOnChange}
+                        formSubmit={this.formSubmit}
+                        imageOnClick={this.imageOnClick}
+                        notFoundMessage={this.state.notFoundMessage}
+                        onOpenModal={this.onOpenModal}
+                        onCloseModal={this.onCloseModal}
+                        open={this.state.open}
+                    />
+                    <TransactionList transactions={this.state.transactions}/>
+                </section>
+            </div>
         );
     }
 
     onOpenModal = () => {
-        this.setState({ open: true });
+        this.setState({open: true});
     };
 
     onCloseModal = () => {
-        this.setState({ open: false });
+        this.setState({open: false});
     };
 
-    amountOnChange = (value) =>{
+    amountOnChange = (value) => {
         this.setState({
-            amount:value
+            amount: value
         })
-    }
+    };
 
-    descriptionOnChange = (value) =>{
+    descriptionOnChange = (value) => {
         this.setState({
-            description:value
+            description: value
         })
-    }
+    };
 
-    formSubmit = (event) =>{
-        event.preventDefault()
-        console.log(this.state.sort)
-        this.componentDidMount()
+    formSubmit = (event) => {
+        event.preventDefault();
+        console.log(this.state.sort);
+        this.componentDidMount();
+    };
+
+    imageOnClick = () => {
         this.setState({
-            description:'',
-            amount:'',
+            sort: (this.state.sort !== 1) ? 1 : 2,
         })
+        console.log(this.state.sort);
+        this.componentDidMount();
     }
-
-    imageOnClick= () =>{
-        this.setState({
-            sort: (this.state.sort!==1)? 1:2,
-        })
-        console.log(this.state.sort)
-        this.componentDidMount()
-    }
-
-
-
-
-
-
-
 }
--- src/DetailTransaction/DetailTransactionInput.test.js
+++ src/DetailTransaction/DetailTransactionInput.test.js
@@ -3,42 +3,40 @@ import React from 'react';
 import DetailTransactionInput from './DetailTransactionInput.jsx';
 
 describe('Transaction Input', () => {
-    describe('render', () => {
-        function setup() {
-            const props =
-            {
-                transactions:[],
-                    description:'',
-                amount:'',
-                sort:false,
-                notFoundMessage:'',
-            };
-            return shallow(<DetailTransactionInput />).setProps(props);
-        }
+  describe('render', () => {
+    function setup() {
+      const props = {
+        transactions: [],
+        description: '',
+        amount: '',
+        image: false,
+        notFoundMessage: '',
+      };
+      return shallow(<DetailTransactionInput />).setProps(props);
+    }
 
-        it('should call amountOnChange when the input change ', () => {
-            const callbackFn = jest.fn();
-            const renderedComponent = setup().setProps({ amountOnChange: callbackFn });
+    it('should call amountOnChange when the input change ', () => {
+      const callbackFn = jest.fn();
+      const renderedComponent = setup().setProps({ amountOnChange: callbackFn });
 
-            renderedComponent.find('#amountInput').simulate('change', { target: {} });
-            expect(callbackFn).toHaveBeenCalled();
-        });
-
-        it('should descriptionOnChange when the input description is change', function () {
-            const callbackFn = jest.fn();
-            const renderedComponent = setup().setProps({ descriptionOnChange: callbackFn });
-
-            renderedComponent.find('#descriptionInput').simulate('change', { target: {} });
-            expect(callbackFn).toHaveBeenCalled();
-        });
-
-        it('should formSubmit when form has been submitted', function () {
-            const callbackFn = jest.fn();
-            const renderedComponent = setup().setProps({ formSubmit: callbackFn });
-
-            renderedComponent.find('form').simulate('submit');
-            expect(callbackFn).toHaveBeenCalled();
-        });
-
+      renderedComponent.find('#amountInput').simulate('change', { target: {} });
+      expect(callbackFn).toHaveBeenCalled();
     });
+
+    it('should descriptionOnChange when the input description is change', () => {
+      const callbackFn = jest.fn();
+      const renderedComponent = setup().setProps({ descriptionOnChange: callbackFn });
+
+      renderedComponent.find('#descriptionInput').simulate('change', { target: {} });
+      expect(callbackFn).toHaveBeenCalled();
+    });
+
+    it('should formSubmit when form has been submitted', () => {
+      const callbackFn = jest.fn();
+      const renderedComponent = setup().setProps({ formSubmit: callbackFn });
+
+      renderedComponent.find('form').simulate('submit');
+      expect(callbackFn).toHaveBeenCalled();
+    });
+  });
 });
